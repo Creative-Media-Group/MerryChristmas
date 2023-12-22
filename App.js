5880667a26282275ba30d01396b89941605f13f6
tct1234 <42028373+tct123@users.noreply.github.com>
2023-12-22 23:11:33 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 import Svg, { Image as SvgImage } from 'react-native-svg'
 
 export default function App() {
   return (
+    // <ImageBackground source={require("./assets/background.png")} height={"100%"} width={"100%"}>
     <View style={styles.container}>
       <Text>Merry Christmas!</Text>
+      <Button title='Hallo' onPress={playsound}></Button>
       <StatusBar style="auto" />
     </View>
+    // </ImageBackground>
+
   );
 }
-
+function playsound() {
+  alert("playing")
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 100
   },
 });
