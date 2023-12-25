97b4eb9a6717a232a6d6f714d88c76dcf8cb7130
tct123 <42028373+tct123@users.noreply.github.com>
2023-12-25 18:08:53 +0100

    Update App.js

--- App.js
+++ App.js
@@ -6,11 +6,21 @@ export default function App() {
   return (
     // <ImageBackground source={require("./assets/background.png")} height={"100%"} width={"100%"}>
     <View style={styles.container}>
-      <Text>Merry Christmas!</Text>
-      <Button title='Hallo' onPress={playsound} style={styles.hellobutton}></Button>
-      <Button title='Red' onPress={redbutton} style={styles.hellobutton}></Button>
-      <Button title='Green' onPress={greenbutton} style={styles.hellobutton}></Button>
-      <Button title='Blue' onPress={bluebutton} style={styles.hellobutton}></Button>
+      <View style={styles.btn}>
+        <Text>Merry Christmas!</Text>
+      </View>
+      <View style={styles.btn}>
+        <Button title='Hallo' onPress={playsound}></Button>
+      </View>
+      <View style={styles.btn}>
+        <Button title='Red' onPress={redbutton} style={styles.hellobutton}></Button>
+      </View>
+      <View style={styles.btn}>
+        <Button title='Green' onPress={greenbutton} style={styles.hellobutton}></Button>
+      </View>
+      <View style={styles.btn}>
+        <Button title='Blue' onPress={bluebutton} style={styles.hellobutton}></Button>
+      </View>
       <StatusBar style="auto" />
     </View>
     // </ImageBackground>
@@ -33,10 +43,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'yellow',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    padding: 10
+
   },
-  hellobutton: {
-    flex: 1
-    //margin: "100px"
+  btn: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    margin: 0
+    //padding: 1
   },
 });
