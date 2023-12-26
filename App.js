4bff80f1787f609df965e8c88f125ad5c2537d58
tct123 <42028373+tct123@users.noreply.github.com>
2023-12-26 03:12:54 +0100

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 import Svg, { Image as SvgImage } from 'react-native-svg'
 
+const text = "todo";
+
 export default function App() {
   return (
     // <ImageBackground source={require("./assets/background.png")} height={"100%"} width={"100%"}>
@@ -10,7 +12,7 @@ export default function App() {
         <Text>Merry Christmas!</Text>
       </View>
       <View style={styles.btn}>
-        <Button title='Hallo' onPress={playsound}></Button>
+        <Button title='Play gingle' onPress={playsound}></Button>
       </View>
       <View style={styles.btn}>
         <Button title='Red' onPress={redbutton} style={styles.hellobutton}></Button>
@@ -30,18 +32,21 @@ function playsound() {
   alert("playing")
 };
 function redbutton() {
-  styles.container.backgroundColor = "red"
+  styles.container.backgroundColor = "red",
+  alert(text)
 };
 function greenbutton() {
-  styles.container.backgroundColor = "green"
+  styles.container.backgroundColor = "green",
+  alert(text)
 };
 function bluebutton() {
-  styles.container.backgroundColor = "blue"
+  styles.container.backgroundColor = "blue",
+  alert(text)
 };
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
+    backgroundColor: '#B22222',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 10
@@ -49,7 +54,7 @@ const styles = StyleSheet.create({
   },
   btn: {
     flex: 1,
-    backgroundColor: 'yellow',
+    //backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
 
