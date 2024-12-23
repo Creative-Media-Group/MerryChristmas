a32e30ad9599ae77c7c28c7d6216d45f88c39e0a
tct123 <42028373+tct123@users.noreply.github.com>
2024-12-23 19:45:38 +0100

    .

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,11 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+  var locale = Intl.DateTimeFormat().resolvedOptions().locale;
+  var lang = locale.split("-")[0];
+  var region = locale.split("-")[1];
   return (
+    // <AppBar>Hello</AppBar>
     <View
       style={{
         flex: 1,
@@ -9,6 +13,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Button title="Hello"></Button>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
